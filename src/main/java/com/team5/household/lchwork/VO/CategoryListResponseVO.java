adf93a40ef6d9e4cff76cbc8a8d625c5d7e2e27e
dlcksgml123123 <qwedcxza0501@naver.com>
2023-02-15 15:49:42 +0900

    lch

---
+++ src/main/java/com/team5/household/lchwork/VO/CategoryListResponseVO.java
@@ -0,0 +1,24 @@
+package com.team5.household.lchwork.VO;
+
+import java.util.List;
+
+import com.team5.household.lchwork.entity.LchCultureCategoryEntity;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CategoryListResponseVO {
+    @Schema(description = "상태", example = "true")
+    private Boolean status; 
+    @Schema(description = "메세지", example = "카테고리 리스트 조회에 성공했습니다.")
+    private String message; 
+    @Schema(description = "추가된 대분류 카테고리 정보")
+    private List<LchCultureCategoryEntity> cclist;
+}
---
+++ src/main/java/com/team5/household/lchwork/VO/CategoryUpdateResponseVO.java
@@ -0,0 +1,23 @@
+package com.team5.household.lchwork.VO;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CategoryUpdateResponseVO {
+    @Schema(description = "수정 상태", example = "true")
+    private Boolean updated;
+    @Schema(description = "메세지", example = "카테고리 정보가 수정되었습니다.")
+    private String message;
+    @Schema(description = "선택한 카테고리 번호", example = "3")
+    private Long no;
+    @Schema(description = "수정할 카테고리 이름", example = "영화")
+    private String name;
+    
+}
