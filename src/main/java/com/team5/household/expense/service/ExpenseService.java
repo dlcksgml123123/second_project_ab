80d0211a63f92355b56ecae499c0061ccc2d9d52
parkmawani <xkclxk@ync.ac.kr>
2023-02-17 17:02:52 +0900

    파일

--- src/main/java/com/team5/household/expense/service/ExpenseService.java
+++ src/main/java/com/team5/household/expense/service/ExpenseService.java
@@ -123,7 +123,7 @@ public class ExpenseService {
     {
         Path imgLocation = Paths.get(expense_img_path+"/"+filename).normalize();
         Resource r = new UrlResource(imgLocation.toUri());
-
+        System.out.println(r.getFilename());
         String contentType = "application/octet-stream";
         // contentType = "image/*";
 
