85e3e782b21a064da7d35af1a63e5f842ea99cd1
parkmawani <xkclxk@ync.ac.kr>
2023-02-24 18:02:59 +0900

    업데이트

--- src/main/java/com/team5/household/expense/repository/MemberPaymentInfoRepository.java
+++ src/main/java/com/team5/household/expense/repository/MemberPaymentInfoRepository.java
@@ -4,5 +4,5 @@ import com.team5.household.expense.entity.MemberPaymentInfoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberPaymentInfoRepository extends JpaRepository<MemberPaymentInfoEntity, Long> {
-    MemberPaymentInfoEntity findByMiSeq(Long seq);
+//    MemberPaymentInfoEntity findByMiSeq(Long seq);
 }
